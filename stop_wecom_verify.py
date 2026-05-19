17d96bc228dab2b7c300ff0f2454830d208b8578
HeJingbo <hejingbo@example.com>
2026-05-19 14:29:40 +0800

    chore: 清理临时文件和旧备份

--- stop_wecom_verify.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('47.96.158.178', username='root', password='REN01250099q', timeout=15)
-ssh.exec_command('pkill -f wecom_verify.py 2>/dev/null')
-print('验证服务已关闭')
-ssh.close()
